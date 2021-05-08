88e8f8a81eec21dbb8e9433c219565c415f9562a
ekee305 <ethan.kee@btinternet.com>
2021-05-08 12:07:33 +0100

    change in depth function

--- src/kinect_package/src/kinect_subscriber.cpp
+++ src/kinect_package/src/kinect_subscriber.cpp
@@ -5,7 +5,7 @@
 #include "geometry_msgs/Point.h"
 
 float x,y;
-sensor_msgs::ImageConstPtr depth_image;
+sensor_msgs::Image depth_image;
 
 bool new_x_y=false;
 bool new_image=false;
@@ -15,21 +15,21 @@ typedef union U_FloatParse {
     unsigned char byte_data[4];
 } U_FloatConvert;
 
-int ReadDepthData(unsigned int height_pos, unsigned int width_pos, sensor_msgs::ImageConstPtr depth_image)
+int ReadDepthData(unsigned int height_pos, unsigned int width_pos, sensor_msgs::Image depth_image)
 {
     // If position is invalid
-    if ((height_pos >= depth_image->height) || (width_pos >= depth_image->width))
+    if ((height_pos >= depth_image.height) || (width_pos >= depth_image.width))
         return -1;
-    int index = (height_pos*depth_image->step) + (width_pos*(depth_image->step/depth_image->width));
+    int index = (height_pos*depth_image.step) + (width_pos*(depth_image.step/depth_image.width));
     // If data is 4 byte floats (rectified depth image)
-    if ((depth_image->step/depth_image->width) == 4) {
+    if ((depth_image.step/depth_image.width) == 4) {
         U_FloatConvert depth_data;
         int i, endian_check = 1;
         // If big endian
-        if ((depth_image->is_bigendian && (*(char*)&endian_check != 1)) ||  // Both big endian
-           ((!depth_image->is_bigendian) && (*(char*)&endian_check == 1))) { // Both lil endian
+        if ((depth_image.is_bigendian && (*(char*)&endian_check != 1)) ||  // Both big endian
+           ((!depth_image.is_bigendian) && (*(char*)&endian_check == 1))) { // Both lil endian
             for (i = 0; i < 4; i++)
-                depth_data.byte_data[i] = depth_image->data[index + i];
+                depth_data.byte_data[i] = depth_image.data[index + i];
             // Make sure data is valid (check if NaN)
             if (depth_data.float_data == depth_data.float_data)
                 return int(depth_data.float_data*1000);
@@ -37,7 +37,7 @@ int ReadDepthData(unsigned int height_pos, unsigned int width_pos, sensor_msgs::
         }
         // else, one little endian, one big endian
         for (i = 0; i < 4; i++) 
-            depth_data.byte_data[i] = depth_image->data[3 + index - i];
+            depth_data.byte_data[i] = depth_image.data[3 + index - i];
         // Make sure data is valid (check if NaN)
         if (depth_data.float_data == depth_data.float_data)
             return int(depth_data.float_data*1000);
@@ -46,11 +46,11 @@ int ReadDepthData(unsigned int height_pos, unsigned int width_pos, sensor_msgs::
     // Otherwise, data is 2 byte integers (raw depth image)
    int temp_val;
    // If big endian
-   if (depth_image->is_bigendian)
-       temp_val = (depth_image->data[index] << 8) + depth_image->data[index + 1];
+   if (depth_image.is_bigendian)
+       temp_val = (depth_image.data[index] << 8) + depth_image.data[index + 1];
    // If little endian
    else
-       temp_val = depth_image->data[index] + (depth_image->data[index + 1] << 8);
+       temp_val = depth_image.data[index] + (depth_image.data[index + 1] << 8);
    // Make sure data is valid (check if NaN)
    if (temp_val == temp_val)
        return temp_val;
@@ -64,7 +64,7 @@ void xyCallback(const geometry_msgs::Point::ConstPtr& msg) {
 }
 
 // Image Callback
-void imageCallback(const sensor_msgs::ImageConstPtr& image) {
+void imageCallback(const sensor_msgs::Image& image) {
     depth_image=image;
     new_image=true;
 }
